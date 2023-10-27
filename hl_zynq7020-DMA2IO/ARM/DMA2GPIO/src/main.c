b184953f7ac2147a93545eb320ce20e69e4c2daf
kulya97 <1942186814@qq.com>
2023-10-27 18:12:08 +0800

    添加注释；

--- hl_zynq7020-DMA2IO/ARM/DMA2GPIO/src/main.c
+++ hl_zynq7020-DMA2IO/ARM/DMA2GPIO/src/main.c
@@ -1,100 +1,58 @@
-/******************************************************************************
- * Copyright (C) 2018 - 2022 Xilinx, Inc.  All rights reserved.
- * SPDX-License-Identifier: MIT
- ******************************************************************************/
-
-/*****************************************************************************/
-/**
- *
- * @file xaxidma_example_sgcyclic_intr.c
- *
- * This file demonstrates how to use the xaxidma driver on the Xilinx AXI
- * DMA core (AXIDMA) to transfer packets in interrupt mode when the AXIDMA
- * core is configured in Scatter Gather Mode
- *
- * This example demonstrates how to use cyclic DMA mode feature.
- * This program will recycle the NUMBER_OF_BDS_TO_TRANSFER
- * buffer descriptors to specified number of cyclic transfers defined in
- * "NUMBER_OF_CYCLIC_TRANSFERS".
- *
- * This code assumes a loopback hardware widget is connected to the AXI DMA
- * core for data packet loopback.
- *
- * To see the debug print, you need a Uart16550 or uartlite in your system,
- * and please set "-DDEBUG" in your compiler options. You need to rebuild your
- * software executable.
- *
- *
- * <pre>
- * MODIFICATION HISTORY:
- *
- * Ver   Who  Date     Changes
- * ----- ---- -------- -------------------------------------------------------
- * 9.4   adk  25/07/17 Initial version.
- * 9.6   rsp  02/14/18 Support data buffers above 4GB.Use UINTPTR for storing
- *                     and typecasting buffer address(CR-992638).
- * 9.8   rsp  07/24/18 Set TX DMACR[Cyclic BD enable] before starting DMA
- *                     operation i.e. in TxSetup.
- * 9.9   rsp  01/21/19 Fix use of #elif check in deriving DDR_BASE_ADDR.
- *       rsp  02/05/19 For test completion wait for both TX and RX done counters.
- * 9.10  rsp  09/17/19 Fix cache maintenance ops for source and dest buffer.
- * 9.14  sk   03/08/22 Delete DDR memory limits comments as they are not
- * 		       relevant to this driver version.
- * 9.15  sa   08/12/22 Updated the example to use latest MIG cannoical define
- * 		       i.e XPAR_MIG_0_C0_DDR4_MEMORY_MAP_BASEADDR.
- *
- * </pre>
- *
- * ***************************************************************************
- */
 /***************************** Include Files *********************************/
 #include "xaxidma.h"
 #include "xparameters.h"
 #include "xil_exception.h"
 #include "xdebug.h"
-
+#include "strings.h"
 extern void xil_printf(const char *format, ...);
 #include "xscugic.h"
 
 /******************** Constant Definitions **********************************/
-/*
- * Device hardware build related constants.
- */
+
 #define DMA_DEV_ID		XPAR_AXIDMA_0_DEVICE_ID
 #define RX_INTR_ID		XPAR_FABRIC_AXIDMA_0_S2MM_INTROUT_VEC_ID
 #define TX_INTR_ID		XPAR_FABRIC_AXIDMA_0_MM2S_INTROUT_VEC_ID
 #define INTC_DEVICE_ID          XPAR_SCUGIC_SINGLE_DEVICE_ID
 
-#warning CHECK FOR THE VALID DDR ADDRESS IN XPARAMETERS.H, \
-			DEFAULT SET TO 0x01000000
-
+//#warning CHECK FOR THE VALID DDR ADDRESS IN XPARAMETERS.H, \
+//			DEFAULT SET TO 0x01000000
+/************************************************************/
+//地址
 #define MEM_BASE_ADDR		0x01000000
+#define TX_BD_SPACE_BASE	(MEM_BASE_ADDR + 0x00100000)
+#define TX_BD_SPACE_HIGH	(MEM_BASE_ADDR + 0x01FFFFFF)
+#define TX_BUFFER_BASE		(MEM_BASE_ADDR + 0x10000000)
+
 #define RX_BD_SPACE_BASE	(MEM_BASE_ADDR)
-#define RX_BD_SPACE_HIGH	(MEM_BASE_ADDR + 0x0000FFFF)
-#define TX_BD_SPACE_BASE	(MEM_BASE_ADDR + 0x00010000)
-#define TX_BD_SPACE_HIGH	(MEM_BASE_ADDR + 0x0001FFFF)
-#define TX_BUFFER_BASE		(MEM_BASE_ADDR + 0x00100000)
-#define RX_BUFFER_BASE		(MEM_BASE_ADDR + 0x00300000)
-#define RX_BUFFER_HIGH		(MEM_BASE_ADDR + 0x004FFFFF)
+#define RX_BD_SPACE_HIGH	(MEM_BASE_ADDR + 0x000FFFFF)
+#define RX_BUFFER_BASE		(MEM_BASE_ADDR + 0x03000000)
+#define RX_BUFFER_HIGH		(MEM_BASE_ADDR + 0x04FFFFFF)
 
-//超时计数
-#define RESET_TIMEOUT_COUNTER	10000
-
+/************************************************************/
+/*
+ * AXI DMA操作需要先提供一个在内存中驻留的不变空间，用于存储需要进行的DMA操作。
+ * 形容这“每一次操作”的东西叫做Buffer Descriptor，缩写叫BD，
+ * 这些BD是连接成链表的形式的，因为BD会动态增加，而预先分配存储BD的空间是恒定的，
+ * 因此BD被连成一个环（BD Ring）,其实就是一个循环链表。
+ *
+ * {packet1    },{packet2    },{packet3    }... n个pkt
+ * {pkt len    },{pkt len    },{pkt len    }...每个pkt的大小由pktlen确定
+ * {bd1,bd2,bd3},{bd1,bd2,bd3},{bd1,bd2,bd3}...每个pkt由n个bd组成，bd的数量
+ *
+ ************************************************************/
 // 要传输的每个packet大小
-#define MAX_PKT_LEN		0x100//缓冲个数
-#define MARK_UNCACHEABLE        0x701
-
+//？？？！！！是否称为每个bd的内存大小更为合适
+#define MAX_PKT_LEN		0x1000   //单位bit
 // 每个packet对应的BD数量
 #define NUMBER_OF_BDS_PER_PKT		2
 // 一共要传输的packet个数
-#define NUMBER_OF_PKTS_TO_TRANSFER 	10
-
+#define NUMBER_OF_PKTS_TO_TRANSFER 	0x10
 // 总共需要的BD总数
-#define NUMBER_OF_BDS_TO_TRANSFER	(NUMBER_OF_PKTS_TO_TRANSFER * \
-						NUMBER_OF_BDS_PER_PKT)
-
-#define NUMBER_OF_CYCLIC_TRANSFERS	100
-
+#define NUMBER_OF_BDS_TO_TRANSFER	(NUMBER_OF_PKTS_TO_TRANSFER *NUMBER_OF_BDS_PER_PKT)
+/************************************************************/
+//超时计数
+#define RESET_TIMEOUT_COUNTER	10000
+/************************************************************/
 /*中断合并阈值和延迟定时器阈值
  *有效范围为1到255
  *我们将合并阈值设置为包的总数。在这个例子中，接收端只会得到一个完成中断。
@@ -106,10 +64,6 @@ extern void xil_printf(const char *format, ...);
 #define INTC_HANDLER	XScuGic_InterruptHandler
 
 /**************************** Type Definitions *******************************/
-
-/***************** Macros (Inline Functions) Definitions *********************/
-
-/************************** Function Prototypes ******************************/
 static void TxCallBack(XAxiDma_BdRing * TxRingPtr);
 static void TxIntrHandler(void *Callback);
 static void RxCallBack(XAxiDma_BdRing * RxRingPtr);
@@ -119,13 +73,11 @@ static int SetupIntrSystem(INTC * IntcInstancePtr, XAxiDma * AxiDmaPtr,
 		u16 TxIntrId, u16 RxIntrId);
 static void DisableIntrSystem(INTC * IntcInstancePtr, u16 TxIntrId,
 		u16 RxIntrId);
-
 static int RxSetup(XAxiDma * AxiDmaInstPtr);
 static int TxSetup(XAxiDma * AxiDmaInstPtr);
 static int SendPacket(XAxiDma * AxiDmaInstPtr);
 
 /************************** Variable Definitions *****************************/
-
 //设备实例
 XAxiDma AxiDma;
 //中断控制器实例
@@ -137,7 +89,15 @@ volatile int Error;
 
 //发送数据包缓冲区，必须32位对齐使用
 u32 *Packet = (u32 *) TX_BUFFER_BASE;
+
+u8 *txchar = "C100(C200(C300(H10L20),C200(H20L10)),C100(H1L5))";
+
+void tx_tg(u8 *txchar) {
+
+}
+
 /*****************************************************************************/
+
 int main(void) {
 	int Status;
 	XAxiDma_Config *Config;
@@ -199,18 +159,6 @@ int main(void) {
 }
 
 /*****************************************************************************/
-/*
- *
- * This is the DMA TX callback function to be called by TX interrupt handler.
- * This function handles BDs finished by hardware.
- *
- * @param	TxRingPtr is a pointer to TX channel of the DMA engine.
- *
- * @return	None.
- *
- * @note		None.
- *
- ******************************************************************************/
 static void TxCallBack(XAxiDma_BdRing * TxRingPtr) {
 	XAxiDma_Bd *BdPtr;
 
@@ -219,21 +167,6 @@ static void TxCallBack(XAxiDma_BdRing * TxRingPtr) {
 }
 
 /*****************************************************************************/
-/*
- *
- * This is the DMA TX Interrupt handler function.
- *
- * It gets the interrupt status from the hardware, acknowledges it, and if any
- * error happens, it resets the hardware. Otherwise, if a completion interrupt
- * presents, then it calls the callback function.
- *
- * @param	Callback is a pointer to TX channel of the DMA engine.
- *
- * @return	None.
- *
- * @note		None.
- *
- ******************************************************************************/
 static void TxIntrHandler(void *Callback) {
 	XAxiDma_BdRing *TxRingPtr = (XAxiDma_BdRing *) Callback;
 	u32 IrqStatus;
@@ -298,21 +231,6 @@ static void RxCallBack(XAxiDma_BdRing * RxRingPtr) {
 }
 
 /*****************************************************************************/
-/*
- *
- * This is the DMA RX interrupt handler function
- *
- * It gets the interrupt status from the hardware, acknowledges it, and if any
- * error happens, it resets the hardware. Otherwise, if a completion interrupt
- * presents, then it calls the callback function.
- *
- * @param	Callback is a pointer to RX channel of the DMA engine.
- *
- * @return	None.
- *
- * @note		None.
- *
- ******************************************************************************/
 static void RxIntrHandler(void *Callback) {
 	XAxiDma_BdRing *RxRingPtr = (XAxiDma_BdRing *) Callback;
 	u32 IrqStatus;
@@ -433,19 +351,6 @@ static void DisableIntrSystem(INTC * IntcInstancePtr, u16 TxIntrId,
 }
 
 /*****************************************************************************/
-/*
- *
- * This function sets up RX channel of the DMA engine to be ready for packet
- * reception
- *
- * @param	AxiDmaInstPtr is the pointer to the instance of the DMA engine.
- *
- * @return	- XST_SUCCESS if the setup is successful.
- *		- XST_FAILURE if fails.
- *
- * @note		None.
- *
- ******************************************************************************/
 static int RxSetup(XAxiDma * AxiDmaInstPtr) {
 	XAxiDma_BdRing *RxRingPtr;
 	int Status;
@@ -554,19 +459,6 @@ static int RxSetup(XAxiDma * AxiDmaInstPtr) {
 }
 
 /*****************************************************************************/
-/*
- *
- * This function sets up the TX channel of a DMA engine to be ready for packet
- * transmission.
- *
- * @param	AxiDmaInstPtr is the pointer to the instance of the DMA engine.
- *
- * @return	- XST_SUCCESS if the setup is successful.
- *		- XST_FAILURE otherwise.
- *
- * @note		None.
- *
- ******************************************************************************/
 static int TxSetup(XAxiDma * AxiDmaInstPtr) {
 	XAxiDma_BdRing *TxRingPtr = XAxiDma_GetTxRing(&AxiDma);
 	XAxiDma_Bd BdTemplate;
@@ -639,8 +531,11 @@ static int SendPacket(XAxiDma * AxiDmaInstPtr) {
 	int Status;
 	int Index, Pkts;
 	UINTPTR BufferAddr;
+	//读取单个pkt报文的最大长度
+	u32 maxtransferlen = TxRingPtr->MaxTransferLen;
 
-	//限制报文长度
+	//限制单个pkt报文长度
+	//单个pkg长度要小于最大传输长度，bdlen*bdnum
 	if (MAX_PKT_LEN * NUMBER_OF_BDS_PER_PKT > TxRingPtr->MaxTransferLen) {
 
 		xil_printf("Invalid total per packet transfer length for the "
@@ -653,29 +548,29 @@ static int SendPacket(XAxiDma * AxiDmaInstPtr) {
 	TxPacket = (u32 *) Packet;
 
 	Value = 0x0;
-
+	//每个bd长度*总共的bd数
 	for (Index = 0; Index < MAX_PKT_LEN * NUMBER_OF_BDS_TO_TRANSFER / 4;
 			Index = Index + 1) {
-		if (Index % 5 == 1 && Index < 100)
+//		if (Index % 5 == 1 && Index < 4096*200)
+//			Value = 0xFFFFFFFF;
+//		else if (Index % 2 == 1 && Index >= 4096*200 && Index < 4096*500)
+//			Value = 0xFFFFFFFF;
+//		else if (Index % 200 == 1 && Index >= 4096*500 && Index < 4096*900)
+//			Value = 0xFFFFFFFF;
+//		else if (Index >= 4096*900 && Index < 4096*1200)
+//			Value = 0xFFFFFFFF;
+//		else if (Index >= 4096*1200 && Index < 4096*1500)
+//			Value = 0x0;
+//		else if (Index >= 4096*1500 && Index < 4096*1800)
+//			Value = 0xFFFFFFFF;
+		if (Index % 5 == 1)
 			Value = 0xFFFFFFFF;
-		else if (Index % 2 == 1 && Index >= 100 && Index < 200)
-			Value = 0xFFFFFFFF;
-		else if (Index >= 200 && Index < 500)
-			Value = 0xFFFFFFFF;
-		else if (Index >= 500 && Index < 700)
-			Value = 0x0;
-		else if (Index >= 700 && Index < 900)
-			Value = 0xFFFFFFFF;
-//		else if (Index >= 100 && Index < 300)
-//			Value = 0x0000;
 //		else if (Index >= 300 && Index < 600)
 //			Value = 0xFFFF;
 		else
 			Value = 0x0;
 		TxPacket[Index] = Value;
 	}
-
-
 	//发送之前刷新缓冲区
 	Xil_DCacheFlushRange((UINTPTR) TxPacket, MAX_PKT_LEN *
 	NUMBER_OF_BDS_TO_TRANSFER);
@@ -697,19 +592,19 @@ static int SendPacket(XAxiDma * AxiDmaInstPtr) {
 	 * Set up the BD using the information of the packet to transmit
 	 * Each transfer has NUMBER_OF_BDS_PER_PKT BDs
 	 */
+	//遍历每一个pkt
 	for (Index = 0; Index < NUMBER_OF_PKTS_TO_TRANSFER; Index++) {
-
+		//遍历每个pkt下的每一个bd
 		for (Pkts = 0; Pkts < NUMBER_OF_BDS_PER_PKT; Pkts++) {
 			u32 CrBits = 0;
-
-			//设置缓冲区地址
+			//设置bd缓冲区地址
 			Status = XAxiDma_BdSetBufAddr(BdCurPtr, BufferAddr);
 			if (Status != XST_SUCCESS) {
 				xil_printf("Tx set buffer addr %x on BD %x failed %d\r\n",
 						(unsigned int) BufferAddr, (UINTPTR) BdCurPtr, Status);
 				return XST_FAILURE;
 			}
-			//设置长度字段
+			//设置bd长度字段
 			Status = XAxiDma_BdSetLength(BdCurPtr, MAX_PKT_LEN,
 					TxRingPtr->MaxTransferLen);
 			if (Status != XST_SUCCESS) {
@@ -731,9 +626,11 @@ static int SendPacket(XAxiDma * AxiDmaInstPtr) {
 			}
 
 			XAxiDma_BdSetCtrl(BdCurPtr, CrBits);
-			XAxiDma_BdSetId(BdCurPtr, BufferAddr);
 
+			XAxiDma_BdSetId(BdCurPtr, BufferAddr);
+			//移动bd指针到下一个bd位置
 			BufferAddr += MAX_PKT_LEN;
+			//依次添加bd到bdring
 			BdCurPtr = (XAxiDma_Bd *) XAxiDma_BdRingNext(TxRingPtr, BdCurPtr);
 		}
 	}
